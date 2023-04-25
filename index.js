1f81ac45c42025ab41d0f8dc4679efa014cf4669
Robot <aomaymistov@gmail.com>
2023-04-25 16:03:54 +0300

    Сделал CRUD для статей

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+import mongoose from "mongoose";
 
-app.get('/', (req, res) => {
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from "./validation.js";
+import checkAuth from "./utils/checkAuth.js";
+import { getMe, login, register } from "./controllers/UserController.js";
+import {create, getAll, getOne, remove, update} from "./controllers/PostController.js";
 
-})
+mongoose
+  .connect(
+    "mongodb+srv://robot:848966@cluster0.vq4n7.mongodb.net/blog?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB OK"))
+  .catch((err) => console.log("DB Error", err));
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/auth/login", loginValidation, login);
+
+app.post("/auth/register", registerValidation, register);
+
+app.get("/auth/me", checkAuth, getMe);
+
+app.get("/posts", getAll);
+app.get("/posts/:id", getOne);
+app.post("/posts", checkAuth, postCreateValidation, create);
+app.delete("/posts/:id", checkAuth, remove);
+app.patch("/posts/:id", update);
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("Server OK");
+});
